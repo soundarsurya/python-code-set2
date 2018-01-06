0dfcd264a28468940d7aab8d0a1437253de976d2
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-06 23:23:22 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 #TO FIND FACTORIAL OF GIVEN NUMBER
-num=int(input("Enter a number N: "))
-res=0
+num=int(input("Enter a number to find factorial: "))
+res=1
 if num>0:
 	for x in range(1,num+1):
-		res=res+x
-		print (res)
+		 res=res*x
+	print (res)
 else:
 	print ("Enter a positive number")
