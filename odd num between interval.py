cc9ad9779bfa9d426bf73bc4dfadac63689259ed
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-27 12:34:07 +0530

    Create odd num between interval.py

---
+++ odd num between interval.py
@@ -0,0 +1,8 @@
+start=int(input("Enter start number: "))
+end=int(input("Enter end number: "))
+if start and end <=100000:
+	for x in range(start+1,end):
+		if x%2 !=0:
+			print(x)
+else:
+	print("Enter a valid number")
