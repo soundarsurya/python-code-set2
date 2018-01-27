36e9a33b9a0c0ace70484006f0b82fefd09b4ef7
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-27 12:41:25 +0530

    Update even num between interval.py

--- even num between interval.py
+++ even num between interval.py
@@ -2,7 +2,7 @@ start=int(input("Enter start number: "))
 end=int(input("Enter end num: "))
 if start and end <100000:
 	for x in range(start+1,end):
-    	if x%2 ==0:
-      		print(x)
+		if x%2 ==0:
+			print(x)
 else:
-  	print("Enter valid number")
+	print("Enter a valid number")
