bb0b1f7c765a5bd4cdea7a6f8b4eae87b161ddc2
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-27 11:46:11 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,5 @@
-num = input('Enter a number b/w 1 & 1000 : ')
-if num<=1000:
-	if num == str(num)[::-1]:
-		print('yes')
-	else:
-        		print('no')
+num = input('Enter any number : ')
+if num == str(num)[::-1]:
+    print('yes')
 else:
-	print("enter a valid number")
+    print('No')
