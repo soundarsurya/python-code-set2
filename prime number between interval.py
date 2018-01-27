1e8364711c368682f0212eadd66ea965bb192d89
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-27 12:46:24 +0530

    Update prime number between interval.py

--- prime number between interval.py
+++ prime number between interval.py
@@ -1,1 +1,10 @@
-
+start=int(input("Enter start number: "))
+end=int(input("Enter end number: "))
+for x in range(start+1,end):
+	for y in range(2,start,end):
+		if num%y == 0:
+			print (y)
+	else:
+    		print("Yes")
+else:
+print("enter a valid number")
